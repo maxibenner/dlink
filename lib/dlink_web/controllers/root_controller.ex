f137be1b8f749e7b3e81250fdda63d3f51bcdc61
Maximilian Benner <maxibenner@gmail.com>
2025-12-17 19:32:44 +0100

    feat: updated static

--- lib/dlink_web/controllers/root_controller.ex
+++ lib/dlink_web/controllers/root_controller.ex
@@ -1,14 +1,14 @@
 defmodule DlinkWeb.RootController do
   use DlinkWeb, :controller
 
-  @index_path :dlink
-               |> :code.priv_dir()
-               |> to_string()
-               |> Path.join("static/index.html")
-
   def show(conn, _params) do
+    index_path =
+      :dlink
+      |> :code.priv_dir()
+      |> Path.join("static/index.html")
+
     conn
     |> put_resp_content_type("text/html; charset=utf-8")
-    |> send_file(200, @index_path)
+    |> send_file(200, index_path)
   end
 end
